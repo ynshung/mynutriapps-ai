b4882199ed87ce141e6b5f1a641f5329c94122a8
Young Shung <me@ynshung.com>
2025-04-10 03:56:44 +0800

    fix minor mistake

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def nutritional_table_detection_preview():
                 ),
                 400,
             )
-        return jsonify({"status": "success", "data": result, "uuid": })
+        return jsonify({"status": "success", "data": result, "uuid": boxes_uuid})
     except Exception as e:
         return jsonify({"status": str(e)}), 500
 
